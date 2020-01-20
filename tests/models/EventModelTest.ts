284138b22caa308c917b0eac03d9b22697069ccc
Laurent Cozic <laurent@cozic.net>
2020-01-20 01:47:04 +0000

    Improved tests

--- tests/models/EventModelTest.ts
+++ tests/models/EventModelTest.ts
@@ -15,9 +15,9 @@ describe('EventModelTest', function() {
 		const eventModel = new EventModel();
 		const baseProps:Event = { name: 'test', job_id: 'test', hash: '123' };
 
-		const event1:Event = await eventModel.save({ ...baseProps, body: 'one' }); await msleep(1);
-		const event2:Event = await eventModel.save({ ...baseProps, body: 'two' }); await msleep(1);
-		const event3:Event = await eventModel.save({ ...baseProps, body: 'three' }); await msleep(1);
+		await eventModel.save({ ...baseProps, body: 'one' }); await msleep(1);
+		await eventModel.save({ ...baseProps, body: 'two' }); await msleep(1);
+		await eventModel.save({ ...baseProps, body: 'three' }); await msleep(1);
 
 		{
 			const events = await eventModel.eventsSince('test', 0, []);
@@ -39,21 +39,22 @@ describe('EventModelTest', function() {
 		const event1:Event = await eventModel.save({ ...baseProps, body: 'one' }, saveOptions);
 		const event2:Event = await eventModel.save({ ...baseProps, body: 'two' }, saveOptions);
 		const event3:Event = await eventModel.save({ ...baseProps, body: 'three' }, saveOptions);
+		const event4:Event = await eventModel.save({ ...baseProps, body: 'four', created_time: 2000, updated_time: 2000 }, saveOptions);
 
 		{
 			const events = await eventModel.eventsSince('test', 1000, []);
-			expect(events.length).toBe(3);
+			expect(events.length).toBe(4);
 		}
 
 		{
 			const events = await eventModel.eventsSince('test', 1000, [event1.id]);
-			expect(events.length).toBe(2);
+			expect(events.length).toBe(3);
 		}
 
 		{
-			const events = await eventModel.eventsSince('test', 1000, [event1.id, event3.id]);
+			const events = await eventModel.eventsSince('test', 1000, [event1.id, event2.id, event3.id]);
 			expect(events.length).toBe(1);
-			expect(events[0].id).toBe(event2.id);
+			expect(events[0].id).toBe(event4.id);
 		}
 	}));
 
