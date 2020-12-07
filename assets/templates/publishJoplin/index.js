446cbfa95d473d939c406e0fdf17988a8a787f7c
Laurent Cozic <laurent@cozic.net>
2020-12-07 18:59:46 +0000

    event scripts

--- assets/templates/publishJoplin/index.js
+++ assets/templates/publishJoplin/index.js
@@ -1,34 +1,41 @@
 exports = {
-	run: async function(events) {
-		const apiToken = events.params.apiToken;
-		const apiPort = events.params.apiPort;
-		const notebookTitle = events.params.notebookTitle;
-
-		console.info('IIIIIIIIIIIII', apiToken, apiPort, notebookTitle);
+	run: async function(context) {
+		const apiToken = context.params.apiToken;
+		const apiPort = context.params.apiPort;
+		const folderTitle = context.params.folderTitle;
 
 		const baseUrl = `http://127.0.0.1:${apiPort}`;
 
-		const response = await fetch(`${baseUrl}/folders?token=${apiToken}`);
-		console.info(await response.json());
+		let folder = null;
+		let response = null;
 
-		// for (let event of events) {
-		// 	const body = JSON.parse(event.body);
-		// 	if (!body) throw new Error('Invalid event: ' + JSON.parse(event));
+		do {
+			const responseRaw = await fetch(`${baseUrl}/folders?token=${apiToken}`);
+			response = await responseRaw.json();
+			folder = response.items.find(n => n.title === folderTitle);
+			if (folder) break;
+		} while (response.has_more);
 
-		// 	const r = await fetch('http://127.0.0.1:' + apiPort + '/notes?token=' + apiToken, {
-		// 		method: 'POST',
-		// 		body: JSON.stringify({
-		// 			title: body.title,
-		// 			body: '* Author: ' + body.author + '\n* URL: ' + body.url + '\n\n* * *' + body.body,
-		// 			parent_id: 'bad5f51a4b564d42a27c2dcab1c69878',
-		// 			user_created_time: event.created_time,
-		// 			user_updated_time: event.updated_time,
-		// 			is_todo: 1,
-		// 		}),
-		// 		headers: { 'Content-Type': 'application/json' },
-		// 	});
+		if (!folder) throw new Error(`Could not find a notebook titled "${folderTitle}"`);
 
-		// 	if (!r.ok) throw new Error('Could not post to Joplin: ' + await r.text());
-		// }
+		const event = context.event;
+
+		const body = JSON.parse(event.body);
+		if (!body) throw new Error(`Invalid event: ${JSON.parse(event)}`);
+
+		const r = await fetch(`${baseUrl}/notes?token=${apiToken}`, {
+			method: 'POST',
+			body: JSON.stringify({
+				title: body.title,
+				body: `* Author: ${body.author}\n* URL: ${body.url}\n\n* * *\n\n${body.body}`,
+				parent_id: folder.id,
+				user_created_time: event.created_time,
+				user_updated_time: event.updated_time,
+				is_todo: 1,
+			}),
+			headers: { 'Content-Type': 'application/json' },
+		});
+
+		if (!r.ok) throw new Error(`Could not post to Joplin: ${await r.text()}`);
 	},
 };
